dbe2698e0953d374ae7f5c2ca039b096af2eea40
AligramTelegram <103333098+AligramTelegram@users.noreply.github.com>
2026-06-07 10:40:06 +0300

    chore: remove 78 one-off generator/fixer scripts

--- _check_schema.py
+++
@@ -1,7 +0,0 @@
-import re, sys
-sys.stdout.reconfigure(encoding='utf-8')
-c = open(r'C:\Users\muham\Desktop\APICalculators\blog\llm-api-cost-guide-2026.html', encoding='utf-8').read()
-schemas = re.findall(r'<script type="application/ld\+json">(.*?)</script>', c, re.DOTALL)
-for i, s in enumerate(schemas):
-    print(f'Schema {i+1}:', s[:300])
-    print()
